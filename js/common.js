814e65a5514a519cd925312104a16763b3d73f0c
Molly-Lotusdream <2694117916@qq.com>
2022-06-08 05:03:32 +0800

    final

---
+++ js/common.js
@@ -0,0 +1,45 @@
+window.onload = function() {
+	//footer样式修改及界面跳转
+	var hzlogo = document.getElementById('HZlogo');
+	hzlogo.onmouseover = function colorchange1(e) {
+		hzlogo.style.backgroundColor = "#007562";
+	};
+	hzlogo.onmouseout = function colorchange1(e) {
+		hzlogo.style.backgroundColor = "#aaa";
+	};
+	var twitterlogo = document.getElementById('twitterlogo');
+	twitterlogo.onmouseover = function colorchange1(e) {
+		twitterlogo.style.backgroundColor = "#1da1f2";
+	};
+	twitterlogo.onmouseout = function colorchange1(e) {
+		twitterlogo.style.backgroundColor = "#aaa";
+	};
+
+	var facebooklogo = document.getElementById('facebooklogo');
+	facebooklogo.onmouseover = function colorchange1(e) {
+		facebooklogo.style.backgroundColor = "#1877f2";
+	};
+	facebooklogo.onmouseout = function colorchange1(e) {
+		facebooklogo.style.backgroundColor = "#aaa";
+	};
+
+	var youtubelogo = document.getElementById('youtubelogo');
+	youtubelogo.onmouseover = function colorchange1(e) {
+		youtubelogo.style.backgroundColor = "#ff0000";
+	};
+	youtubelogo.onmouseout = function colorchange1(e) {
+		youtubelogo.style.backgroundColor = "#aaa";
+	};
+	var rsslogo = document.getElementById('rsslogo');
+	rsslogo.onmouseover = function colorchange1(e) {
+		rsslogo.style.backgroundColor = "#f26522";
+	};
+	rsslogo.onmouseout = function colorchange1(e) {
+		rsslogo.style.backgroundColor = "#aaa";
+	};
+
+
+
+
+
+};
---
+++ js/ideas.js
@@ -0,0 +1,5 @@
+var left = document.getElementById('u-left');
+var right = document.getElementById('u-right');
+left.onmousedown= function (e) {
+	console.log('3');
+}
